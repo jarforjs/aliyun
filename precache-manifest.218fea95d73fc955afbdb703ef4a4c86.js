34d6da9c84611fce878e502a1d5c75933b7255a8
林风 <guhongbo@zhujia360.com>
2019-12-22 22:13:58 +0800

    Updates

---
+++ precache-manifest.218fea95d73fc955afbdb703ef4a4c86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fff47f4ded01073b823eef49026cf1",
+    "url": "/aliyun/index.html"
+  },
+  {
+    "revision": "e8cf002217cd501cdb9f",
+    "url": "/aliyun/static/css/2.43f3662a.chunk.css"
+  },
+  {
+    "revision": "fe369fd9b85381953151",
+    "url": "/aliyun/static/css/main.9e25dc40.chunk.css"
+  },
+  {
+    "revision": "e8cf002217cd501cdb9f",
+    "url": "/aliyun/static/js/2.57eadc7c.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/aliyun/static/js/2.57eadc7c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fe369fd9b85381953151",
+    "url": "/aliyun/static/js/main.06c54a68.chunk.js"
+  },
+  {
+    "revision": "53c7d87c767b13984c9f",
+    "url": "/aliyun/static/js/runtime-main.98aec603.js"
+  }
+]);
